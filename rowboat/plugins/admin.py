9804bf3a2653db09ca7d2f49b9bf7d9f2b53b9a5
DeJay <contact@dejaydev.com>
2019-12-25 13:42:46 -0600

    sigh

--- rowboat/plugins/admin.py
+++ rowboat/plugins/admin.py
@@ -1094,12 +1094,11 @@ class AdminPlugin(Plugin):
             reason=reason or 'no reason',
         )
 
-
-                if rated[0][0] > 40:
-                    if len(rated) == 1:
-                        role_obj = rated[0][1]
-                    elif rated[0][0] - rated[1][0] > 20:
-                        role_obj = rated[0][1]
+        if rated[0][0] > 40:
+            if len(rated) == 1:
+                role_obj = rated[0][1]
+            elif rated[0][0] - rated[1][0] > 20:
+                role_obj = rated[0][1]
 
         if not role_obj:
             raise CommandFail('too many matches for that role, try something more exact or the role ID')
