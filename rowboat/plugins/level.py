119571b5a5b9b0cabc0a899090e2d2848ef22509
DeJay <contact@dejaydev.com>
2020-02-11 11:00:49 -0600

    messy commit

--- rowboat/plugins/level.py
+++ rowboat/plugins/level.py
@@ -8,6 +8,7 @@ from rowboat.types import Field
 from rowboat.types.plugin import PluginConfig
 from rowboat.models.tags import Tag
 from rowboat.models.user import User, XPBlock
+from rowboat.models.guild import GuildMemberLevel
 
 class LevelPluginConfig(PluginConfig):
     when_ur_a = 'a'
@@ -24,7 +25,7 @@ class LevelPlugin(Plugin):
                 return False
             raise CommandFail('Cannot execute that action on yourself')
     
-    @Plugin.command('block', '<user:user|snowflake> [reason:str...]', group='xp', aliases=['mute', 'stfu'], level=CommandLevels.MOD)
+    @Plugin.command('block', '<user:user|snowflake> [reason:str...]', group='xp2', aliases=['mute', 'stfu'], level=CommandLevels.MOD)
     def xp_block(self, event, user, reason):
         member = event.guild.get_member(user)
         if member:
@@ -46,7 +47,7 @@ class LevelPlugin(Plugin):
 
         raise CommandSuccess(u'Blocked {} from gaining XP.'.format(name))
 
-    @Plugin.command('unblock', '<user:user|snowflake> [reason:str...]', group='xp', aliases=['unmute'], level=CommandLevels.MOD)
+    @Plugin.command('unblock', '<user:user|snowflake> [reason:str...]', group='xp2', aliases=['unmute'], level=CommandLevels.MOD)
     def xp_unblock(self, event, user, reason):
         member = event.guild.get_member(user)
         if member:
@@ -68,32 +69,58 @@ class LevelPlugin(Plugin):
     @Plugin.command('reset', '<user:user|snowflake>',
     #aliases=[],
     context={'action': 'reset'},
-    group='xp',
+    group='xp2',
     level=CommandLevels.ADMIN)
     @Plugin.command('give', '<user:user|snowflake> <amount:int>',
     aliases=['add'],
     context={'action': 'give'},
-    group='xp', 
+    group='xp2', 
     level=CommandLevels.ADMIN)
     @Plugin.command('take', '<user:user|snowflake> <amount:int>',
     aliases=['remove'],
     context={'action': 'take'},
-    group='xp',
+    group='xp2',
     level=CommandLevels.ADMIN)
-    def xp_edit(self, event, user, action=None):
+    def xp_edit(self, event, user, amount=None, action=None):
         member = event.guild.get_member(user)
 
         if not member:
             raise CommandFail('Invalid member')
 
+        if not isinstance(amount, ((int),(long)):
+            raise CommandFail('Invalid amount')
+
         self.can_act_on(event, member.id)
 
         if action == 'give':
-            #Add
+            user = GuildMemberLevel.get().where(
+                (GuildMemberLevel.user_id == member.id) &
+                (GuildMemberLevel.guild_id == member.guild_id)
+            ).execute()
+
+            user.add_xp(amount)
+
+            raise CommandSuccess(u'{} was given {} XP. (New Total: `{}`)'.format(
+                member.alias,
+                amount,
+                'in dev' #Get Current Amount (do above and just pull as var w/ this amt+ added)
+            ))
         elif action == 'take':
-            #Remove
+            user = GuildMemberLevel.get().where(
+                (GuildMemberLevel.user_id == member.id) &
+                (GuildMemberLevel.guild_id == member.guild_id)
+            ).execute()
+
+            user.rmv_xp(amount)
+            
+            raise CommandSuccess(u'Took {} XP from {}. (New Total: `{}`)'.format(
+                amount,
+                member.alias,
+                #Get Current Amount (do above and just pull as var w/ this amt+ added)
+            ))
         else:
             #Reset to 0
+            #Do the gevent confirm action
 
         #TODO: Modlog call :)
 
