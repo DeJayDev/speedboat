e372cbf80c1916ca1c1a20324c3a1b6450c1e90d
DeJay <contact@dejaydev.com>
2020-04-27 01:37:53 -0500

    getting this file was harder than it should've been

--- rowboat/__init__.py
+++ rowboat/__init__.py
@@ -7,7 +7,7 @@ from disco.util.logging import LOG_FORMAT
 from sentry_sdk.integrations.redis import RedisIntegration
 from yaml import safe_load
 
-with open('../config.yaml', 'r') as f:
+with open(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir) + '/config.yaml')) as f: # Please tell me how to fix this
     config = safe_load(f)
 
 ENV = config['ENV']
