dae0acfc590860a7c72d2c8e0a9e88862264d0f8
DeJay <contact@dejaydev.com>
2020-04-27 01:29:24 -0500

    fix whoopsie 2

--- rowboat/__init__.py
+++ rowboat/__init__.py
@@ -7,7 +7,7 @@ from disco.util.logging import LOG_FORMAT
 from sentry_sdk.integrations.redis import RedisIntegration
 from yaml import safe_load
 
-with open('config.yaml', 'r') as f:
+with open('../config.yaml', 'r') as f:
     config = safe_load(f)
 
 ENV = config['ENV']
