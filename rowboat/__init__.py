804f305f066fe2fd2279eff024edafa9b7d59d05
DeJay <contact@dejaydev.com>
2020-10-15 22:09:57 -0500

    fuck travis

--- rowboat/__init__.py
+++ rowboat/__init__.py
@@ -7,6 +7,10 @@ from disco.util.logging import LOG_FORMAT
 from sentry_sdk.integrations.redis import RedisIntegration
 from yaml import safe_load
 
+# Please tell me how to fix this
+with open(os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir) + '/config.yaml')) as f:
+    config = safe_load(f)
+
 ENV = config['ENV']
 DSN = config['DSN']
 REV = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
