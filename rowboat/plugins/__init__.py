58857bcb140f9227448525e5d11c6e786fd790a9
DeJay <contact@dejaydev.com>
2021-11-17 19:19:09 -0600

    fucking

--- rowboat/plugins/__init__.py
+++ rowboat/plugins/__init__.py
@@ -3,6 +3,7 @@ from disco.bot import Plugin
 from disco.bot.command import CommandEvent
 from disco.bot.plugin import register_plugin_base_class
 from disco.gateway.events import GatewayEvent
+from disco.types.base import Unset
 
 from rowboat import sentry
 from rowboat.types import Field
@@ -96,7 +97,7 @@ class RowboatPlugin(SentryPlugin, Plugin):
     def with_config(cls, config_cls):
         def deco(plugin_cls):
             name = plugin_cls.__name__.replace('Plugin', '').lower()
-            PluginsConfig._fields[name] = Field(config_cls)
+            PluginsConfig._fields[name] = Field(config_cls, default=Unset)
             PluginsConfig._fields[name].name = name
             # PluginsConfig._fields[name].default = None
             return plugin_cls
