a824193983e024f8c02e5ef4725a54aa2a88b343
DeJayDev <contact@dejaydev.com>
2023-06-24 18:59:31 -0500

    Remove unused constant
    Closes DeJayDev/speedboat#51

--- rowboat/constants.py
+++ rowboat/constants.py
@@ -39,22 +39,7 @@ SNOOZE_EMOJI = "\U0001f4a4"
 # Regexes
 INVITE_LINK_RE = re.compile(r"(?:http|https)?(?::)?(?://)?((?:dsc|dis|discord|invite).(?:gd|gg|io|me)/([a-zA-Z0-9\-]+))", re.I)
 URL_RE = re.compile(r"(https?://[^\s]+)")
-EMOJI_PATTERN = re.compile(
-    "["
-    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-    "\U0001F300-\U0001F5FF"  # symbols & pictographs
-    "\U0001F600-\U0001F64F"  # emoticons
-    "\U0001F680-\U0001F6FF"  # transport & map symbols
-    "\U0001F700-\U0001F77F"  # alchemical symbols
-    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
-    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
-    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
-    "\U0001FA00-\U0001FA6F"  # Chess Symbols
-    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
-    "\U00002702-\U000027B0"  # Dingbats
-    "\U000024C2-\U0001F251"
-    "]+"
-)
+
 EMOJI_RE = re.compile(r"<a?:(.+):([0-9]+)>")
 USER_MENTION_RE = re.compile("<@!?([0-9]+)>")
 
